574d626e01fbd838a47d4803a6169b8be3d9b712
pukekaka <pukekakaster@gmail.com>
2017-11-10 10:46:33 +0900

    commit

--- practice/mann/mann.py
+++ practice/mann/mann.py
@@ -25,17 +25,18 @@ def omniglot():
                                   max_shift=0.,
                                   max_iter=None)
 
-    it = iter(generator)
-
-    f = open("test", 'w')
-    while True:
-        try:
-            e = next(it)
-            f.write(str(e))
-        except StopIteration:
-            break
-    f.close()
-    print('end')
+    # it = iter(generator)
+    #
+    # f = open("test", 'w')
+    # while True:
+    #     try:
+    #         e = next(it)
+    #         f.write(str(e))
+    #     except StopIteration:
+    #         break
+    # f.close()
+    # print('end')
+    print (generator)
 
 if __name__ == '__main__':
     omniglot()
